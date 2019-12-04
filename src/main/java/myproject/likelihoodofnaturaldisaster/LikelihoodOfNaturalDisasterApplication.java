70413bcceef76604c6984d951e8aa5364dd0231d
Alex Katorzhin <alex.katorzhin@gmail.com>
2019-12-04 20:37:35 +0200

    Initial commit

---
+++ src/main/java/myproject/likelihoodofnaturaldisaster/LikelihoodOfNaturalDisasterApplication.java
@@ -0,0 +1,13 @@
+package myproject.likelihoodofnaturaldisaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelihoodOfNaturalDisasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikelihoodOfNaturalDisasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/myproject/likelihoodofnaturaldisaster/LikelihoodOfNaturalDisasterApplicationTests.java
@@ -0,0 +1,13 @@
+package myproject.likelihoodofnaturaldisaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikelihoodOfNaturalDisasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
