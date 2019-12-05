a2a3b3f830612bb8fe472a860d8132c06ab9949a
Alex Katorzhin <alex.katorzhin@gmail.com>
2019-12-05 16:09:45 +0200

    first application in spring

--- src/main/java/myproject/likelihoodofnaturaldisaster/LikelihoodOfNaturalDisasterApplication.java
+++ src/main/java/myproject/likelihoodofnaturaldisaster/LikelihoodOfNaturalDisasterApplication.java
@@ -2,12 +2,20 @@ package myproject.likelihoodofnaturaldisaster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LikelihoodOfNaturalDisasterApplication {
+public class LikelihoodOfNaturalDisasterApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(LikelihoodOfNaturalDisasterApplication.class);
+    }
 
     public static void main(String[] args) {
-        SpringApplication.run(LikelihoodOfNaturalDisasterApplication.class, args);
+        SpringApplication.run(LikelihoodOfNaturalDisasterApplication.class,args);
     }
+
 
 }
