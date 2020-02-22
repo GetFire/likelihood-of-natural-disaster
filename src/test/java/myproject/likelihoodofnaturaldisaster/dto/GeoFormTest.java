757aafdfb02e7a1c7f31f28ce4cee770deee2b34
Denys Azhymov <22999487+GetFire@users.noreply.github.com>
2020-02-23 00:33:35 +0200

    [NO TRELLO] Add simple (stupid) unit test (#4)

--- src/test/java/myproject/likelihoodofnaturaldisaster/dto/GeoFormTest.java
+++ src/test/java/myproject/likelihoodofnaturaldisaster/dto/GeoFormTest.java
@@ -1,14 +1,46 @@
 package myproject.likelihoodofnaturaldisaster.dto;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(MockitoJUnitRunner.class)
 public class GeoFormTest {
 
+    private static final String LATITUDE = "12341234";
+    public static final String LONGITUDE = "33322111";
+    private GeoForm testEntry;
+
+    @Before
+    public void setUp() {
+        testEntry = new GeoForm();
+    }
+
     @Test
-    public void shouldAllowToCreateTestedInstance() {
-        GeoForm tested = new GeoForm();
+    public void shouldCreateTestedInstance() {
+        assertThat(testEntry).isNotNull();
+    }
+
+    @Test
+    public void shouldSetLatitude() {
+        testEntry.setLatitude(LATITUDE);
+        assertThat(testEntry.getLatitude()).isEqualTo(LATITUDE);
+    }
+
+    @Test
+    public void shouldSetLongitude() {
+        testEntry.setLongitude(LONGITUDE);
+        assertThat(testEntry.getLongitude()).isEqualTo(LONGITUDE);
+    }
+
+    @Test
+    public void shouldSetLongitudeAndLatitude() {
+        testEntry.setLongitude(LONGITUDE);
+        testEntry.setLatitude(LATITUDE);
+        assertThat(testEntry.getLongitude()).isEqualTo(LONGITUDE);
+        assertThat(testEntry.getLatitude()).isEqualTo(LATITUDE);
     }
 }
