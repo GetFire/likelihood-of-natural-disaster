c29c3244327c808be8d51012a078a2f194646641
Denys Azhymov <denys_azhymov@epam.com>
2020-02-23 00:07:13 +0200

    - add code coverage tool - add simple test

---
+++ src/test/java/myproject/likelihoodofnaturaldisaster/dto/GeoFormTest.java
@@ -0,0 +1,14 @@
+package myproject.likelihoodofnaturaldisaster.dto;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GeoFormTest {
+
+    @Test
+    public void shouldAllowToCreateTestedInstance() {
+        GeoForm tested = new GeoForm();
+    }
+}
