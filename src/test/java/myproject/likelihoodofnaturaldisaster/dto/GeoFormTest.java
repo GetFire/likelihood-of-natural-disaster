6bb07f18ad01896fac43aaa74efb414c73b7d75c
Denys Azhymov <denys_azhymov@epam.com>
2020-02-22 21:04:29 +0200

    [TEST-001] test PR for Travis

---
+++ src/test/java/myproject/likelihoodofnaturaldisaster/dto/GeoFormTest.java
@@ -0,0 +1,18 @@
+package myproject.likelihoodofnaturaldisaster.dto;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.InjectMocks;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GeoFormTest {
+
+
+    @Test
+    public void shouldCreateTestInstance() {
+        GeoForm test = new GeoForm();
+    }
+}
