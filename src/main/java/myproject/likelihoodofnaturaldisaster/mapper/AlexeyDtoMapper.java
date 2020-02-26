7af204cd479a9e024d634db9ccc5fa65be8784bd
Alex Katorzhin <alex.katorzhin@gmail.com>
2020-02-26 17:55:07 +0200

    [NUT-015] created mapper

--- src/main/java/myproject/likelihoodofnaturaldisaster/mapper/AlexeyDtoMapper.java
+++ src/main/java/myproject/likelihoodofnaturaldisaster/mapper/AlexeyDtoMapper.java
@@ -3,34 +3,31 @@ package myproject.likelihoodofnaturaldisaster.mapper;
 import com.github.dvdme.ForecastIOLib.FIODataPoint;
 import myproject.likelihoodofnaturaldisaster.dto.AlexeyForecastDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "spring")
 public interface AlexeyDtoMapper {
-//    AlexeyForecastDto sourceToDestination(FIODataPoint source);
-//
-//    FIODataPoint destinationToSource(AlexeyForecastDto destination);
-default AlexeyForecastDto map(FIODataPoint source) {
-    AlexeyForecastDto target = new AlexeyForecastDto();
-    target.setTime(source.time());
-    target.setSummary(source.summary());
-    target.setSunriseTime(source.sunriseTime());
-    target.setSunsetTime(source.sunsetTime());
-    target.setIcon(source.icon());
-    target.setTimezone(source.getTimezone());
-    target.setTemperatureMinTime(source.temperatureMinTime());
-    target.setTemperatureMaxTime(source.temperatureMaxTime());
-    target.setPrecipType(source.precipType());
-    target.setApparentTemperatureMinTime(source.apparentTemperatureMinTime());
-    target.setApparentTemperatureMaxTime(source.apparentTemperatureMaxTime());
-    target.setPrecipIntensity(source.precipIntensity());
-    target.setCloudCover(source.cloudCover());
-    target.setHumidity(source.humidity());
-    target.setNearestStormDistance(source.nearestStormDistance());
-    target.setPrecipAccumulation(source.precipAccumulation());
-    target.setPressure(source.pressure());
-    target.setVisibility(source.visibility());
-    target.setWindSpeed(source.windSpeed());
-    return target;
-}
+    @Mappings({
+            @Mapping(target = "time", expression = "java(s.time())"),
+            @Mapping(target = "summary", expression = "java(s.summary())"),
+            @Mapping(target = "sunriseTime", expression = "java(s.sunriseTime())"),
+            @Mapping(target = "sunsetTime", expression = "java(s.sunsetTime())"),
+            @Mapping(target = "icon", expression = "java(s.icon())"),
+            @Mapping(target = "temperatureMinTime", expression = "java(s.temperatureMinTime())"),
+            @Mapping(target = "temperatureMaxTime", expression = "java(s.temperatureMaxTime())"),
+            @Mapping(target = "precipType", expression = "java(s.precipType())"),
+            @Mapping(target = "apparentTemperatureMinTime", expression = "java(s.apparentTemperatureMinTime())"),
+            @Mapping(target = "apparentTemperatureMaxTime", expression = "java(s.apparentTemperatureMaxTime())"),
+            @Mapping(target = "precipIntensity", expression = "java(s.precipIntensity())"),
+            @Mapping(target = "cloudCover", expression = "java(s.cloudCover())"),
+            @Mapping(target = "humidity", expression = "java(s.humidity())"),
+            @Mapping(target = "nearestStormDistance", expression = "java(s.nearestStormDistance())"),
+            @Mapping(target = "precipAccumulation", expression = "java(s.precipAccumulation())"),
+            @Mapping(target = "pressure", expression = "java(s.pressure())"),
+            @Mapping(target = "visibility", expression = "java(s.visibility())"),
+            @Mapping(target = "windSpeed", expression = "java(s.windSpeed())"),
+    })
+    AlexeyForecastDto map(FIODataPoint s);
 }
 
